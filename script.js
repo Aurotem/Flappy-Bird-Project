dd672ce606e825f5204069a9443551e90b9e2151
Melihcan Fidan <melihcan.fi@gmail.com>
2023-11-20 22:57:07 +0300

    Physics update

--- script.js
+++ script.js
@@ -62,40 +62,39 @@ function createItems() {
     scenery.appendChild(pipe2);
 
     //? MONITOR
-    let birdPos = bird.style.top.replace("px", "");
-    let pipePos = pipe.style.top.replace("px", "");
-    let pipe2Pos = pipe2.style.top.replace("px", "");
+    let pipeMon = pipe.style.top.replace("px", "");
+    let pipe2Mon = pipe2.style.top.replace("px", "");
     const monitor = document.createElement("div");
     const monitor2 = document.createElement("div");
-    monitor.style.right = '0px';
-    monitor2.style.right = '0px';
-    monitor.id = 'monitor';
-    monitor2.id = 'monitor2';
-    monitor.style.top = pipePos + "px";
-    monitor2.style.top = (Number(pipe2Pos) + 320) + "px";
-    monitor.style.position = 'absolute';
-    monitor2.style.position = 'absolute';
+    monitor.style.right = "0px";
+    monitor2.style.right = "0px";
+    monitor.id = "monitor";
+    monitor2.id = "monitor2";
+    monitor.style.top = pipeMon + "px";
+    monitor2.style.top = Number(pipe2Mon) + 320 + "px";
+    monitor.style.position = "absolute";
+    monitor2.style.position = "absolute";
     monitor.style.background = "red";
-    monitor.style.height = "1px";
+    monitor.style.height = "5px";
     monitor.style.width = "50px";
     monitor2.style.background = "red";
-    monitor2.style.height = "1px";
+    monitor2.style.height = "5px";
     monitor2.style.width = "50px";
     scenery.append(monitor2);
     scenery.append(monitor);
 
     const monitor3 = document.createElement("div");
     const monitor4 = document.createElement("div");
-    monitor3.style.height = '100%';
-    monitor3.style.width = '10px';
-    monitor4.style.height = '100%';
-    monitor4.style.width = '10px';
-    monitor3.style.right = '222px';
-    monitor4.style.right = '154px';
-    monitor3.style.background = 'red';
-    monitor4.style.background = 'red';
-    monitor3.style.position = 'absolute';
-    monitor4.style.position = 'absolute';
+    monitor3.style.height = "100%";
+    monitor3.style.width = "10px";
+    monitor4.style.height = "100%";
+    monitor4.style.width = "10px";
+    monitor3.style.right = "222px";
+    monitor4.style.right = "154px";
+    monitor3.style.background = "red";
+    monitor4.style.background = "red";
+    monitor3.style.position = "absolute";
+    monitor4.style.position = "absolute";
     scenery.append(monitor3);
     scenery.append(monitor4);
     //? MONITOR
@@ -104,6 +103,12 @@ function createItems() {
     let playerPass = false;
     const moveItems = setInterval(() => {
       if (gameRun) {
+        let birdPos = bird.style.top.replace("px", "");
+        let pipePos = pipe.style.top.replace("px", "");
+        let pipe2Pos = pipe2.style.top.replace("px", "");
+        cl("Top pipe: " + (Number(pipe2Pos) + 320));
+        cl("Bird: " + birdPos);
+        cl("Bottom pipe:" + pipePos);
         pipe.style.right = itemsPos + "px";
         pipe2.style.right = itemsPos + "px";
         monitor.style.right = itemsPos + "px";
@@ -115,19 +120,15 @@ function createItems() {
           pipe2.remove();
           clearInterval(moveItems);
         }
-        breakme:if (itemsPos >= 154 && itemsPos <= 222) {
-
-
-          cl(pipePos);
-          cl(birdPos);
-          cl(Number(pipe2Pos)+ 320);
-          if (birdPos <= pipePos && birdPos >= (Number(pipe2Pos) + 320)) {
+        if (!playerPass && itemsPos >= 154 && itemsPos <= 222) {
+          if (birdPos < pipePos && birdPos > Number(pipe2Pos) + 320) {
+            cl('pass')
             playerPass = true;
             if (playerPass) {
               SCORE += 1;
-              playerPass = false;
             }
           } else {
+            cl('fall')
             birdFall();
           }
 
